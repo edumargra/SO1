13ce476431c91c5c6811a824eedd08bd35b233a1
JOAN PERACAULA PRAT <jperacpr7.alumnes@ub052229.mat.ub.edu>
2018-03-02 18:19:01 +0100

    Add ex5 working

--- P1/ex5.sh
+++ P1/ex5.sh
@@ -1,19 +1,22 @@
 if [ $# == 3 ]
 then
-	for f in $(ls $1/*.$2)
+	for f in $(ls $1)
 	do
-		for word in $(cat $f)
-		do
-			if [ $3 == $word ]
-			then
-				echo $1/$f
-				break
-			fi
-		done
-	done
-	for dir in $(ls $1/)
-	do
-		./ex5.sh $1/$dir $2 $3
+        extensio=${f##*.}
+        if [ $extensio == $2 ]
+        then
+            for word in $(cat $1/$f)
+            do
+                if [ $3 == $word ]
+                then
+                    echo $1$f
+                    break
+                fi
+            done
+        elif [ -d $1$f ]
+        then
+            ./ex5.sh $1$f $2 $3
+        fi
 	done
 	exit 0
 else
