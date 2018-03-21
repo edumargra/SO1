563e0f90c1f94ff7c33d7410f47730f06c3f61e9
JoanPeracaula21 <jperacaula1@gmail.com>
2018-03-21 11:56:41 +0100

    Add ex3

---
+++ P2/ex3.sh
@@ -0,0 +1,16 @@
+if [ $# == 2 ]
+then
+    find $1 -type f -exec ls {} \; > files1.txt
+    find $2 -type f -exec ls {} \; | sed "s:$2:$1:g" > files2.txt
+	for file1 in $(cat files1.txt)
+	do
+    	if ! grep -Fxq $file1 files2.txt
+    	then
+    		echo $file1
+    	fi
+    done
+	exit 0
+else
+	echo "ERROR: Introdueix dos parametres: dos directoris."
+	exit 1
+fi
