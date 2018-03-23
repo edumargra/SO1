09db5fa4baf27c721352b141e47d62421307cf89
Eduard Martín Graells <tgn.workday@gmail.com>
2018-03-24 00:28:12 +0100

    entrega 2

--- P2/ex2.sh
+++ P2/ex2.sh
@@ -3,7 +3,13 @@ then
 	suma=0
 	suma=$(ls -lp $1 | grep -v / | awk '{suma += $5} END {print suma}')
 	echo $suma $1
-	find $1 -type d -exec ./ex2.sh {} \;
+	for f in $(ls $1)	
+	do
+	if [ -d $1/$f ]
+        then
+            ./ex2.sh $1/$f
+        fi
+	done
 	exit 0
 else
 	echo "ERROR: Introdueix un unic parametre, un directori."
