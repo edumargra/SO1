b85b8b7efba9013ff2b84059caceeb8ddefcc3e3
eduard <tgn.workday@gmail.com>
2018-05-05 14:05:09 +0200

    arreglat l'exercici

--- Problemes_punters/fitxers/exercici.c
+++ Problemes_punters/fitxers/exercici.c
@@ -4,30 +4,39 @@
 int main () {
    int i;
    void  *ptr;
-   
+
    int var_int[1000];
    double var_double[1000];
-   
+
    for(i = 0; i < 1000; i++) {
        var_int[i] = 2 * i + 1;
        var_double[i] = 2.5 * i + 6.7;
    }
-   
+
    ptr = malloc(1000 * sizeof(int) + 1000 * sizeof(double));
-   
+
    // Emmagatzemnar els valors de var_int i var_double
    // de forma intercalada. Primer var_int[0], seguit
    // de var_double[0], seguit de var_int[1], seguit
    // de var_double[1] i així successivament
 
-   for(i = 0; i < 1000; i++) {
-      *ptr = (int *) (var_int + i);
-      ptr++;
-      *ptr = (double *) (var_double + i);
-      ptr += 2; //double ocupa 2 bytes
-   }
 
-   free(ptr);
 
-   return 0;
-} 
+
+	for(i = 0; i < 1000; i++) {
+		*(int*)ptr = var_int[i];  //canviem el valor emmagatzemat a ptr
+		ptr = (void*)(int*)ptr++; //avancem el pointer per un int
+    printf("Value of var_int[%d] variable: %d\n", i, var_int[i] );
+    printf("Address stored in ptr variable: %d\n", *(int*)ptr );
+
+    *(double*)ptr = var_double[i]; //canviem el valor emmagatzemat a ptr
+		ptr = (void*)(double*)ptr++; //avancem el pointer per un double
+    printf("Value of var_double[%d] variable: %f\n", i, var_double[i] );
+    printf("Address stored in ptr variable: %f\n", *(double*)ptr );
+
+
+	}
+	free(ptr);
+
+	return 0;
+}
